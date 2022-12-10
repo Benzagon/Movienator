a159b2cdad47fad23320c04ba13328cb713d0597
Benzagon <gonbenzaquen@gmail.com>
2022-12-10 19:11:34 -0300

    feat: nav and body

--- movienator/src/assets/pages/flick.jsx
+++ movienator/src/assets/pages/flick.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const flick = () => {
     return (
-        <div>
-            <h2>Hola</h2>
+        <div className="flick">
+            <div className="flick-filters">
+                <button>Filters</button>
+            </div>
+            <div className="flick-center">
+                <h1>Know your next flick?</h1>
+                <button className="red-btn">Flick!</button>
+                <h2>3 free Flicks remaining</h2> 
+
+                <div className="flick-subscribe">
+                    <h3>Subscribe for unlimited Flicks!</h3>
+                </div>
+            </div>
         </div>
     )
 };
--- movienator/src/assets/pages/nav.jsx
+++ movienator/src/assets/pages/nav.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import logo from "./images/logo.svg" 
+import hamburguer from "./images/hamburguer.svg"
+
 const navbar = () => {
     return (
         <div className="nav">
-            <h1>this is nav</h1>
+            <div className="nav-left">
+                <img src={hamburguer} alt="" />
+            </div>
+            <div className="nav-right">
+                <img src={logo} width={75} alt="logo" />
+            </div>
         </div>
     )
 };
